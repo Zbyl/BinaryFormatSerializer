2fcaa3af5a24f2f97c874b34279572bced1eb446
Zbigniew Skowron <zbychs@gmail.com>
2016-01-06 23:42:46 +0100

    Added tests for external_value.

--- tests/VariousTests.cpp
+++ tests/VariousTests.cpp
@@ -2,11 +2,13 @@
 //
 
 #include "VectorSerializer.h"
-#include "ArraySerializer.h"
+#include "MemorySerializer.h"
 
 #include "endian_formatter.h"
 #include "const_formatter.h"
 #include "size_prefix_formatter.h"
+#include "vector_formatter.h"
+#include "external_value.h"
 
 #include "gtest/gtest.h"
 
@@ -40,7 +42,7 @@ TEST(SizePrefixFormatterWorks, SavingAndLoading)
 {
     {
         std::array<uint8_t, 8> data;
-        ArraySaveSerializer vectorWriter(data);
+        MemorySaveSerializer vectorWriter(data);
         save< size_prefix_formatter< little_endian<4>, little_endian<4> > >(vectorWriter, 0x12345678);
         const auto value = std::vector<uint8_t> { 0x04, 0x00, 0x00, 0x00, 0x78, 0x56, 0x34, 0x12 };
         ASSERT_TRUE(( std::equal(value.begin(), value.end(), vectorWriter.getData()) ));
@@ -48,7 +50,7 @@ TEST(SizePrefixFormatterWorks, SavingAndLoading)
 
     {
         uint8_t data[] = { 0x04, 0x00, 0x00, 0x00, 0x78, 0x56, 0x34, 0x12 };
-        ArrayLoadSerializer arrayReader(data, sizeof(data));
+        MemoryLoadSerializer arrayReader(data, sizeof(data));
         int value;
         load< size_prefix_formatter< little_endian<4>, little_endian<4> > >(arrayReader, value);
         EXPECT_EQ(value, 0x12345678);
@@ -56,9 +58,37 @@ TEST(SizePrefixFormatterWorks, SavingAndLoading)
 
     {
         uint8_t data[] = { 0x12, 0x34, 0x56, 0x78 };
-        ArrayLoadSerializer arrayReader(data, sizeof(data));
+        MemoryLoadSerializer arrayReader(data, sizeof(data));
         int value;
         ASSERT_THROW((load< size_prefix_formatter< little_endian<2>, little_endian<2> > >(arrayReader, value)), invalid_data);
+    }
+}
+
+TEST(ExternalValueWorks, SavingAndLoading)
+{
+    {
+        VectorSaveSerializer vectorWriter;
+        int size = 5;
+        std::vector<uint8_t> vec { 0x00, 0x01, 0x02, 0x03, 0x04 };
+        save(vectorWriter, vec, create_vector_formatter(create_external_value(size), little_endian<1>()));
+        const auto value = std::vector<uint8_t> { 0x00, 0x01, 0x02, 0x03, 0x04 };
+        EXPECT_EQ(vectorWriter.getData(), value);
+    }
+
+    {
+        std::vector<uint8_t> data { 0x00, 0x01, 0x02, 0x03, 0x04 };
+        VectorLoadSerializer vectorReader(data);
+        int size = 5;
+        std::vector<uint8_t> vec;
+        load(vectorReader, vec, create_vector_formatter(create_external_value(size), little_endian<1>()));
+        EXPECT_EQ(vec, data);
+    }
+
+    {
+        VectorSaveSerializer vectorWriter;
+        int size = 10;
+        std::vector<uint8_t> vec { 0x00, 0x01, 0x02, 0x03, 0x04 };
+        EXPECT_THROW( (save(vectorWriter, vec, create_vector_formatter(create_external_value(size), little_endian<1>()))), serialization_exception);
     }
 }
 
