1a2035d14383d028326346f17d7d2cff37227e8a
Zbigniew Skowron <zbychs@gmail.com>
2016-01-06 23:30:21 +0100

    Added bit_formatter. Added small template meta programming helper library.

---
+++ source/bit_formatter.h
@@ -0,0 +1,72 @@
+/////////////////////////////////////////////////////////////////////////////
+/// BinaryFormatSerializer
+///    Library for serializing data in arbitrary binary format.
+///
+/// bit_formatter.h
+///
+/// This file contains bit_formatter that stores individual values or tuples of values packed in bitfields.
+///
+/// Distributed under Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
+/// (c) 2014 Zbigniew Skowron, zbychs@gmail.com
+///
+/////////////////////////////////////////////////////////////////////////////
+
+#ifndef BinaryFormatSerializer_bit_formatter_H
+#define BinaryFormatSerializer_bit_formatter_H
+
+#include "endian_formatter.h"
+#include "bit_packer.h"
+
+namespace binary_format
+{
+
+template<boost::endian::order TargetOrder, int... Bits>
+class bit_formatter
+{
+    using packer = bit_packer<Bits...>;
+    using value_formatter = endian_formatter<TargetOrder, sizeof(packer::packed_type)>;
+
+public:
+    template<typename TSerializer, typename... Ts>
+    void save(TSerializer& serializer, Ts... vals) const
+    {
+        packer::packed_type val = packer::pack(vals...);
+        value_formatter().save(serializer, val);
+    }
+
+    template<typename TSerializer, typename... Ts>
+    void load(TSerializer& serializer, Ts&... vals) const
+    {
+        packer::packed_type val;
+        value_formatter().load(serializer, val);
+        packer::unpack(val, vals...);
+    }
+    template<typename TSerializer, typename... Ts>
+    void save(TSerializer& serializer, const std::tuple<Ts...>& vals) const
+    {
+        packer::packed_type val = packer::pack(vals);
+        value_formatter().save(serializer, val);
+    }
+
+    /// Unpacks into a tuple of references.
+    template<typename TSerializer, typename... Ts>
+    void load(TSerializer& serializer, const std::tuple<Ts&...>& vals) const
+    {
+        packer::packed_type val;
+        value_formatter().load(serializer, val);
+        packer::unpack(val, vals);
+    }
+
+    /// Unpacks into a reference to a tuple of values.
+    template<typename TSerializer, typename... Ts>
+    void load(TSerializer& serializer, std::tuple<Ts...>& vals) const
+    {
+        packer::packed_type val;
+        value_formatter().load(serializer, val);
+        packer::unpack(val, vals);
+    }
+};
+
+} // namespace binary_format
+
+#endif // BinaryFormatSerializer_bit_formatter_H
